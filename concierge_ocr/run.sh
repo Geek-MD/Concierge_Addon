32b6efdf81dd37dd009fac82b95fd0e3335cbd0a
Edison Montes M. <edison.montes@gmail.com>
2026-07-20 15:36:07 -0400

    Add API token authentication

--- concierge_ocr/run.sh
+++ concierge_ocr/run.sh
@@ -3,5 +3,12 @@
 OCR_LANG="$(bashio::config 'ocr_lang' 'es')"
 export OCR_LANG
 
+API_TOKEN="$(bashio::config 'api_token' '')"
+if [[ ${#API_TOKEN} -lt 32 ]]; then
+    bashio::log.fatal "api_token must contain at least 32 characters. Generate one with: openssl rand -hex 32"
+    exit 1
+fi
+export API_TOKEN
+
 bashio::log.info "Starting Concierge OCR API on port 8099 (ocr_lang=${OCR_LANG})"
 exec uvicorn app.main:app --host 0.0.0.0 --port 8099
